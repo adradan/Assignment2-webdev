815fcebb8470191e1003f29c323783beeef0ff41
Eric <ericvsolcruz@gmail.com>
2023-03-19 18:45:09 -0400

    Added turn detection

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ function App() {
     const mapService = MapService.Instance;
     const [rows, setRows] = useState(3);
     const [status, setStatus] = useState('');
+    const [turns, setTurns] = useState(0);
+    const [currentTimeout, setCurrentTimeout] =
+        useState<ReturnType<typeof setTimeout>>();
+    // [row, col]
+    const [lastSelection, setLastSelection] = useState([] as number[]);
 
     const genNewMap = () => {
         return mapService.generateMap(rows, colorService.createNewColors(rows));
@@ -24,8 +29,23 @@ function App() {
         const row = coordinate[0];
         const col = coordinate[1];
         const newMap = [...map];
-        newMap[row][col].visible = !newMap[row][col].visible;
+        const currentSelection = newMap[row][col];
+        if (currentSelection.visible) return;
+
+        currentSelection.visible = !currentSelection.visible;
         setMap(newMap);
+
+        if (!lastSelection.length) {
+            setLastSelection([row, col]);
+            return;
+        }
+
+        const lastRow = lastSelection[0];
+        const lastCol = lastSelection[1];
+        const lastInfo = newMap[lastRow][lastCol];
+
+        setTurns(turns + 1);
+        setLastSelection([]);
     };
 
     const rowChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -37,10 +57,13 @@ function App() {
     const newGame = () => {
         const TWO_SECONDS_MS = 2000;
         setMap(genNewMap());
+        setTurns(0);
+        setLastSelection([]);
         setStatus('New Game Created.');
-        setTimeout(() => {
+        const timeout = setTimeout(() => {
             setStatus('');
         }, TWO_SECONDS_MS);
+        setCurrentTimeout(timeout);
     };
 
     useEffect(() => {
@@ -67,7 +90,10 @@ function App() {
                         </button>
                     </div>
                 </div>
-                <div className="app-row">{status}</div>
+                <div className="app-row">
+                    <div>Turns: {turns}</div>
+                    <div>{status}</div>
+                </div>
                 <div className="app-row">
                     <div className="app-map">
                         {map.map((row, rowIdx) => {
