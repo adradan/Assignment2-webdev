5bb92759a8794d81b36ecb0bf264044e08ddf63d
Eric <ericvsolcruz@gmail.com>
2023-03-19 17:39:49 -0400

    New Game button shuffles and creates new cards

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     const colorService = ColorService.Instance;
     const mapService = MapService.Instance;
     const [rows, setRows] = useState(3);
+    const [status, setStatus] = useState('');
 
     const genNewMap = () => {
         return mapService.generateMap(rows, colorService.createNewColors(rows));
@@ -33,8 +34,17 @@ function App() {
         setRows(parseInt(value));
     };
 
-    useEffect(() => {
+    const newGame = () => {
+        const TWO_SECONDS_MS = 2000;
         setMap(genNewMap());
+        setStatus('New Game Created.');
+        setTimeout(() => {
+            setStatus('');
+        }, TWO_SECONDS_MS);
+    };
+
+    useEffect(() => {
+        newGame();
     }, [rows]);
 
     return (
@@ -51,11 +61,13 @@ function App() {
                         <button
                             type="button"
                             className="new-game-btn pointer no-select"
+                            onClick={newGame}
                         >
                             New Game
                         </button>
                     </div>
                 </div>
+                <div className="app-row">{status}</div>
                 <div className="app-row">
                     <div className="app-map">
                         {map.map((row, rowIdx) => {
