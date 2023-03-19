31ec3ba7008474a11ac22a0b0226e0bb2a8efec2
Eric <ericvsolcruz@gmail.com>
2023-03-19 18:33:02 -0400

    Added turn logic and added new game function to button that i forgot abt

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import { Card } from './shared/components/Card/Card.component';
 import { ColorService } from './shared/services/ColorService';
 import { MapService } from './shared/services/MapService';
 import { RowSelect } from './shared/components/RowSelect/RowSelect.component';
+import { Color } from './shared/models/Color.model';
 
 function App() {
     const colorService = ColorService.Instance;
     const mapService = MapService.Instance;
     const [rows, setRows] = useState(3);
+    const [turns, setTurns] = useState(0);
+    // selections type
+    // [[row, col], [row, col]]
+    const [selections, setSelections] = useState([] as number[][]);
 
     const genNewMap = () => {
         return mapService.generateMap(rows, colorService.createNewColors(rows));
@@ -23,8 +28,33 @@ function App() {
         const row = coordinate[0];
         const col = coordinate[1];
         const newMap = [...map];
-        newMap[row][col].visible = !newMap[row][col].visible;
+        const currSelection = newMap[row][col];
+        if (currSelection.visible) return;
+        const toggleVisibility = (
+            currMap: Color[][],
+            currRow: number,
+            currCol: number
+        ) => {
+            currMap[currRow][currCol].visible =
+                !currMap[currRow][currCol].visible;
+        };
+
+        // Show cards first
+        toggleVisibility(newMap, row, col);
         setMap(newMap);
+
+        const lastSelection = selections[0];
+        if (!lastSelection) {
+            const newSelection = [row, col];
+            setSelections([newSelection]);
+            return;
+        }
+        const lastRow = lastSelection[0];
+        const lastCol = lastSelection[1];
+        const lastSelectionInfo = newMap[lastRow][lastCol];
+
+        setSelections([]);
+        setTurns(turns + 1);
     };
 
     const rowChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -33,8 +63,13 @@ function App() {
         setRows(parseInt(value));
     };
 
-    useEffect(() => {
+    const newGame = () => {
         setMap(genNewMap());
+        setTurns(0);
+    };
+
+    useEffect(() => {
+        newGame();
     }, [rows]);
 
     return (
@@ -51,10 +86,14 @@ function App() {
                         <button
                             type="button"
                             className="new-game-btn pointer no-select"
+                            onClick={newGame}
                         >
                             New Game
                         </button>
                     </div>
+                </div>
+                <div className="app-row">
+                    <div>Turns: {turns}</div>
                 </div>
                 <div className="app-row">
                     <div className="app-map">
